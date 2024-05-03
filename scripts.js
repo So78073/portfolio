34c1483abde3e04fd8a619fd4ded7f6423036164
So78073 <srpedroyshisuk1@gmail.com>
2024-05-03 16:36:03 -0300

    02/05/2024 70% do caminho andado

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
+const myObserver = new IntersectionObserver(function (entradas) {
+    entradas.forEach(function(e) {
+        if (e.isIntersecting) {
+            e.target.classList.add('show')
+        } else {
+            e.target.classList.remove('show')
+        }
+    })
+});
+
+const elem = document.querySelectorAll('.hidden');
+
+elem.forEach( function (e) {
+    myObserver.observe(e); 
+})
+
+
+
+
+
+
 document.addEventListener('click', function (e) {
     if (e.target.classList.contains('btProjects')) {
         const project = document.getElementById('Project');
