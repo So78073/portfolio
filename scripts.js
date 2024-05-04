c0e72df60f07343fc33b1087255c021d3581f8b7
So78073 <srpedroyshisuk1@gmail.com>
2024-05-03 23:49:32 -0300

    02/05/2024 75% do caminho andado

--- scripts.js
+++ scripts.js
@@ -17,14 +17,32 @@ elem.forEach( function (e) {
 
 
 
-
+let color = ['#e600ff', '#6557ff']
 
 document.addEventListener('click', function (e) {
+    const sideProjects = document.querySelectorAll('.sideProject');
+    const root = document.documentElement;
+
     if (e.target.classList.contains('btProjects')) {
         const project = document.getElementById('Project');
+
+        color.reverse()
+        const novoGradiente = `linear-gradient(to left, ${color[0]}, ${color[1]})`;
+        root.style.setProperty('--gradiente2Pronject', novoGradiente);
+
         if (e.target.getAttribute('mode') === "L") {
+            
+            sideProjects.forEach(function(elem) {
+                elem.classList.add('animations')
+            })
+            
             project.classList.add('flipRanimationL');
+
         } else {
+            sideProjects.forEach(function(elem) {
+                elem.classList.add('animations')
+                
+            })
             project.classList.add('flipRanimationR');
         }
         
@@ -32,7 +50,13 @@ document.addEventListener('click', function (e) {
         project.addEventListener('animationend', function() {
             this.classList.remove('flipRanimationR');
             this.classList.remove('flipRanimationL');
+            sideProjects.forEach(function(elem) {
+                elem.classList.remove('animations')
+            })
         }, { once: true });
-
     }
 });
+
+function reverseColor() {
+    
+}
