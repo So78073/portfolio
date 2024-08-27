e6c12be18939c11e5a884c9bc44b38785ed7fe62
So78073 <srpedrosyshisuk1@gmail.com>
2024-08-27 17:17:10 -0300

    bug Fixed

--- projects.js
+++ projects.js
@@ -7,7 +7,9 @@ const projects = [
         first_p: 'O desenvolvimento deste portfólio foi extremamente satisfatório de trabalhar. Ver minhas ideias saírem de uma folha A4 para algo verdadeiramente bonito de se ver é uma sensação muito boa!',
 
         second_Htwo: 'Tecnologias usadas',
-        second_p:'No portfólio, utilizei apenas o front-end. Eu até queria implementar alguma API, porém o GitHub Pages não suportaria algo grande. Mas tenho certeza de que posso fazer isso para a sua empresa ou projeto !!'
+        second_p:'No portfólio, utilizei apenas o front-end. Eu até queria implementar alguma API, porém o GitHub Pages não suportaria algo grande. Mas tenho certeza de que posso fazer isso para a sua empresa ou projeto !!',
+        img: "https://so78073.github.io/portfolio/assets/exemples/page001.png"
+    
     },
     {
         title: 'ShizuRender iMG',
@@ -15,7 +17,8 @@ const projects = [
         first_p: 'Um software open source onde você pode redimenssionar centenas de imagens para o padrão que você queira com apenas 1 click !!',
 
         second_Htwo: 'Tecnologias usadas',
-        second_p:'Neste projeto usei Python como back-end e como front utilizei a biblioteca PyQt5. pretendo dar continuidade no software pois ganhei 3h do meu dia com este simples projetos!'
+        second_p:'Neste projeto usei Python como back-end e como front utilizei a biblioteca PyQt5. pretendo dar continuidade no software pois ganhei 3h do meu dia com este simples projetos!',
+        img: "https://so78073.github.io/portfolio/assets/exemples/page002.png"
     }
 ]
 
@@ -44,6 +47,7 @@ function RenderProjectSequence(objProject) {
 
     second_h2 = document.getElementById('second_h2').innerText = objProject.second_Htwo
     second_p = document.getElementById('second_p').innerText = objProject.second_p
+    img = document.getElementById('proj_exemple').src = objProject.img
 
 }
 
